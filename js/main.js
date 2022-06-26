328c621017ade546f9f2516a77fab2fca1490baf
Lucas Medeiros <lucaasmedeiros7@gmail.com>
2022-06-26 17:00:16 -0300

    Finalizando projeto

--- js/main.js
+++ js/main.js
@@ -1,40 +1,53 @@
 const inputTarefa = document.querySelector(".nova-tarefa input");
 const btn = document.querySelector(".nova-tarefa button");
-const lista = document.querySelector(".lista-tarefas");
+const listaDeTarefas = document.querySelector(".lista-tarefas");
+const filtros = document.querySelectorAll(".filtros span");
+const limparTodas = document.querySelector(".btn-limpar");
 
 let idEditado;
 let tarefaFoiEditada = false;
 
 //Pega a lista de tarefas no localStorage
-let listaDeTarefas = JSON.parse(localStorage.getItem("lista-tarefas"));
+let armazenamentoDeTarefas = JSON.parse(localStorage.getItem("lista-tarefas"));
 
-function mostrarTarefa() {
+filtros.forEach((status) => {
+  status.addEventListener("click", () => {
+    document.querySelector("span.active").classList.remove("active");
+    status.classList.add("active");
+    mostrarTarefa(status.id);
+  });
+});
+
+function mostrarTarefa(filtro) {
   let li = "";
 
-  if (listaDeTarefas) {
-    listaDeTarefas.forEach((tarefa, id) => {
+  if (armazenamentoDeTarefas) {
+    armazenamentoDeTarefas.forEach((tarefa, id) => {
       //Se o status da ${tarefa} é completa, coloca o valor checked na variavel
       let statusCompleta = tarefa.status == "completa" ? "checked" : "";
-      li += `<li class="tarefa">
-              <label for="${id}">
-                <input onclick="atualizaStatus(this)" type="checkbox" id="${id}" ${statusCompleta} />
-                <p class="${statusCompleta}">${tarefa.nome}</p>
-              </label>
-              <div class="config">
-                <i onclick="mostrarMenu(this)" class="uil uil-ellipsis-h"></i>
-                <ul class="menu-tarefa">
-                  <li onclick="editaTarefa(${id}, '${tarefa.nome}')"><i class="uil uil-pen"></i>Editar</li>
-                  <li onclick="deletaTarefa(${id})"><i class="uil uil-trash"></i>Delete</li>
-                </ul>
-              </div>
-            </li>`;
+
+      if (filtro == tarefa.status || filtro == "todas") {
+        li += `<li class="tarefa">
+                <label for="${id}">
+                  <input onclick="atualizaStatus(this)" type="checkbox" id="${id}" ${statusCompleta} />
+                  <p class="${statusCompleta}">${tarefa.nome}</p>
+                </label>
+                <div class="config">
+                  <i onclick="mostrarMenu(this)" class="uil uil-ellipsis-h"></i>
+                  <ul class="menu-tarefa">
+                    <li onclick="editaTarefa(${id}, '${tarefa.nome}')"><i class="uil uil-pen"></i>Editar</li>
+                    <li onclick="deletaTarefa(${id})"><i class="uil uil-trash"></i>Delete</li>
+                  </ul>
+                </div>
+              </li>`;
+      }
     });
   }
 
-  lista.innerHTML = li;
+  listaDeTarefas.innerHTML = li || `Você não possui tarefas aqui`;
 }
 
-mostrarTarefa();
+mostrarTarefa("todas");
 
 function mostrarMenu(tarefaSelecionada) {
   let menuTarefa = tarefaSelecionada.parentElement.lastElementChild;
@@ -55,11 +68,18 @@ function editaTarefa(idTarefa, nomeTarefa) {
 }
 
 function deletaTarefa(idTarefa) {
-  //Remove a tarefa do array/listaDeTarefas
-  listaDeTarefas.splice(idTarefa, 1);
-  localStorage.setItem("lista-tarefas", JSON.stringify(listaDeTarefas));
-  mostrarTarefa();
+  //Remove a tarefa do array/armazenamentoDeTarefas
+  armazenamentoDeTarefas.splice(idTarefa, 1);
+  localStorage.setItem("lista-tarefas", JSON.stringify(armazenamentoDeTarefas));
+  mostrarTarefa("todas");
 }
+
+limparTodas.addEventListener("click", () => {
+  //Remove todas as tarefas do array/armazenamentoDeTarefas
+  armazenamentoDeTarefas.splice(0, armazenamentoDeTarefas.length);
+  localStorage.setItem("lista-tarefas", JSON.stringify(armazenamentoDeTarefas));
+  mostrarTarefa();
+});
 
 function atualizaStatus(tarefaSelecionada) {
   //Pega o paragrafo que contem o nome da tarefa
@@ -67,37 +87,43 @@ function atualizaStatus(tarefaSelecionada) {
   if (tarefaSelecionada.checked) {
     //atualiza o status da tarefa selecionada para completa
     nomeTarefa.classList.add("checked");
-    listaDeTarefas[tarefaSelecionada.id].status = "completa";
+    armazenamentoDeTarefas[tarefaSelecionada.id].status = "completa";
   } else {
     //atualiza o status da tarefa selecionada para pendente
     nomeTarefa.classList.remove("checked");
-    listaDeTarefas[tarefaSelecionada.id].status = "pendente";
+    armazenamentoDeTarefas[tarefaSelecionada.id].status = "pendente";
   }
   //Atualiza a informação no localstorage
-  localStorage.setItem("lista-tarefas", JSON.stringify(listaDeTarefas));
+  localStorage.setItem("lista-tarefas", JSON.stringify(armazenamentoDeTarefas));
 }
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  let tarefaUsuario = inputTarefa.value.trim();
+  let tarefaUsuario = inputTarefa.value
+    .trim()
+    .replaceAll("<", "&lt")
+    .replaceAll(">", "&gt");
 
   if (tarefaUsuario) {
     if (!tarefaFoiEditada) {
-      if (!listaDeTarefas) {
-        // Se "listaDeTarefas" não existir, passa um array vazio
-        listaDeTarefas = [];
+      if (!armazenamentoDeTarefas) {
+        // Se "armazenamentoDeTarefas" não existir, passa um array vazio
+        armazenamentoDeTarefas = [];
       }
       let infoTarefa = { nome: tarefaUsuario, status: "pendente" };
-      listaDeTarefas.push(infoTarefa); //add a nova tarefa no array/listaDeTarefas
+      armazenamentoDeTarefas.push(infoTarefa); //add a nova tarefa no array/armazenamentoDeTarefas
     } else {
       tarefaFoiEditada = false;
-      listaDeTarefas[idEditado].nome = tarefaUsuario;
+      armazenamentoDeTarefas[idEditado].nome = tarefaUsuario;
     }
 
     inputTarefa.value = "";
     //O setItem modifica o localstorage: convertendo o objeto para um arquivo JSON
-    localStorage.setItem("lista-tarefas", JSON.stringify(listaDeTarefas));
-    mostrarTarefa();
+    localStorage.setItem(
+      "lista-tarefas",
+      JSON.stringify(armazenamentoDeTarefas)
+    );
+    mostrarTarefa("todas");
   }
 });
